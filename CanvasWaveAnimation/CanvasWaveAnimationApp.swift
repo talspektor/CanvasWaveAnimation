d95132eab9caed5c5fc00c9fd613dac1c775179f
Tal Spektor <tal.s@zemingo.com>
2022-06-25 14:48:28 +0300

    Initial Commit

---
+++ CanvasWaveAnimation/CanvasWaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasWaveAnimationApp.swift
+//  CanvasWaveAnimation
+//
+//  Created by Tal talspektor on 25/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CanvasWaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasWaveAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CanvasWaveAnimation
+//
+//  Created by Tal talspektor on 25/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
